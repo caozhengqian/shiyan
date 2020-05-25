a982bc5b788cde09997afd6e21f14f7a9ac6406c
FHQITS09 <Abcd123456>
2020-05-25 15:27:14 +0800

    左侧菜单栏路由

--- router/vue-router-test/src/router/index.js
+++ router/vue-router-test/src/router/index.js
@@ -1,36 +1,80 @@
-/*
- * @Author: King
- * @Page:
- * @Date: 2020-03-24 15:05:12
- * @Last Modified by: King
- * @Last Modified time: 2020-03-26 14:18:15
- * @FilePath: /src/router/index.js
- */
-import Vue from "vue";
-import VueRouter from "vue-router";
-// 营销活动
-import toRouter from "./module/toRouter";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
-  ...toRouter,
-  {
-    name: "index",
-    path: "/",
-    alias: "/index",
-    component: () => import("../views/OnePage")
-  }
-];
+    {
+        path: '/',
+        component: () => import('../views/OnePage'),
+        meta: {
+            title: 'OnePageTitle'
+        }
+    },
+    {
+        path: '/TwoPagePath',
+        component: () => import('../views/TwoPage'),
+        meta: {
+            title: 'TwoPageTitle'
+        },
+        children: [
+            {
+                path: 'TwoOnePagePath',
+                component: () => import('../components/twos/TwoOnePage'),
+                meta: {
+                    title: 'TwoOnePageTitle'
+                }
+            },
+            {
+                path: 'TwoTwoPagePath',
+                component: () => import('../components/twos/TwoTwoPage'),
+                meta: {
+                    title: 'TwoTwoPageTitle'
+                }
+            },
+            {
+                path: 'TwoThreePagePath',
+                component: () => import('../components/twos/TwoThreePage'),
+                meta: {
+                    title: 'TwoThreePageTitle'
+                }
+            }
+        ]
+    },
+    {
+        path: '/ThreePagePath',
+        component: () => import('../views/ThreePage'),
+        meta: {
+            title: 'ThreePageTitle'
+        },
+        children: [
+            {
+                path: 'ThreeOnePagePath',
+                component: () => import('../components/threes/ThreeOnePage'),
+                meta: {
+                    title: 'ThreeOnePageTitle'
+                }
+            },
+            {
+                path: 'ThreeTwoPagePath',
+                component: () => import('../components/threes/ThreeTwoPage'),
+                meta: {
+                    title: 'ThreeTwoPageTitle'
+                }
+            },
+            {
+                path: 'ThreeThreePagePath',
+                component: () => import('../components/threes/ThreeThreePage'),
+                meta: {
+                    title: 'ThreeThreePageTitle'
+                }
+            }
+        ]
+    },
+]
+
 const router = new VueRouter({
-  mode: "hash", //history hash
-  base: process.env.BASE_URL,
-  routes
-});
+    routes
+})
 
-router.beforeEach((to, from, next) => {
-  console.log(to.params);
-  next();
-});
-
-export default router;
+export { router, routes }
