682dae7b8259a89a729129cd89f9a4e87c77ceee
caozhengqian <1264419544@qq.com>
2020-06-06 03:20:09 +0800

    webpack新增react

--- css/antd-demo/src/index.js
+++ css/antd-demo/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import TodoApp from "./TodoApp";
 import * as serviceWorker from './serviceWorker';
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+        <TodoApp />
+    </Provider>,
   document.getElementById('root')
 );
 
