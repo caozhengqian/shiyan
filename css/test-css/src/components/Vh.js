d43b8976af4c20845c74a461ad2d7c67623bb986
caozhengqian <1264419544@qq.com>
2020-06-07 16:10:55 +0800

    使用慕课网redux和redux插件

---
+++ css/test-css/src/components/Vh.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default class Vh extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div>
+                Vh
+            </div>
+        );
+    }
+}
