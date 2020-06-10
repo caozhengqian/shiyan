ee11b79e8d42d74cc45d0aa70eed435cb514d9a5
caozhengqian <1264419544@qq.com>
2020-06-10 22:38:26 +0800

    使用慕课网redux和redux插件

--- react-compent/src/App.js
+++ react-compent/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import style from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className={style.App}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className={style.aaa}>
+          Edita <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
