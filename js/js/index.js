a5a93bcdcc19be750af070ee89636bca747de47c
caozhengqian <1264419544@qq.com>
2020-07-05 20:09:14 +0800

    position

---
+++ js/js/index.js
@@ -0,0 +1,9 @@
+       abc()   //打印abc
+       function abc() {
+           console.info('abc');
+       }
+
+       abcd()  //abcd is not defined
+       abcd = ()=>{
+           console.info('abcd')
+       }
