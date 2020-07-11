0a62bb86f017cc0fa84fdd1b8daca6795751b222
caozhengqian <1264419544@qq.com>
2020-07-11 20:34:36 +0800

    恢复初始化

--- react-compent/src/App.js
+++ react-compent/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
-import style from './App.module.css';
+import './App.css';
 
 function App() {
   return (
-    <div className={style.App}>
+    <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className={style.aaa}>
-          Edita <code>src/App.js</code> and save to reload.
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
