24354784d584072053f5c3846488309fc173d7be
caozhengqian <1264419544@qq.com>
2020-07-23 01:55:52 +0800

    初始化imoc

--- creact-react-files/com/Test/index.js
+++ creact-react-files/com/Test/index.js
@@ -1,60 +1,65 @@
 import React from 'react';
 import "./index.less";
-import IMG from "./img/fuwuyoushi.png"
-import classnames from "classnames"
 
-export default class Test extends React.Component {
+import { connect } from 'react-redux'
+import { addGun, removeGun, addGunAsync } from '../../redux/index.redux'
+@connect(
+    // 你要state什么属性放到props里
+    state=>({counter:state.counter}),
+    // 你要什么方法，放到props里，自动dispatch
+    { addGun, removeGun, addGunAsync }
+)
+class Test extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            change:true
+        };
     }
-
+    //新建、更新，都会走
+    static getDerivedStateFromProps(){
+        console.info("=>All--getDerivedStateFromProps");
+        return "";
+    }
+    //新建render后
     componentDidMount() {
-
+        // console.info(this.props.counter);
+        console.info("=>All--componentDidMount");
     }
 
+    //更新前,不返回会报错
+    // shouldComponentUpdate(nextProps, nextState){
+    //     console.info("=>All--shouldComponentUpdate");
+    // }
+    //更新render后，
+    getSnapshotBeforeUpdate(){
+        console.info("=>All--getSnapshotBeforeUpdate");
+        return null;
+    }
+    //getSnapshotBeforeUpdate 之后
+    componentDidUpdate(){
+        console.info("=>All--componentDidUpdate");
+    }
+    //卸载前
+    componentWillUnmount(){
+        console.info("=>All--componentWillUnmount");
+    }
+
+    handleClick = (e)=>{
+        this.setState({change:!this.state.change});
+    };
+    handleOn=()=>{
+      this.props.addGun();
+    };
     render() {
         return (
-            <div className="test">
-                <div className="padding">
-                    <div className="paddings">
-                        padding: 10px 20px 50px;
-                    </div>
-                </div>
-                <div className="hong-bao">
-                    <div className="left">
-                        <span className="left-span">abc</span>
-                        <img className="imgs" alt="Vue logo" src={IMG}/>
-                    </div>
-                    <div className="line">
-
-                    </div>
-                    <div className="right">
-                            <span>领取</span>
-                    </div>
-                </div>
-                <div className="text">
-                    <div className="point">
-                        <div className="">
-
-                        </div>
-                    </div>
-                    <div className="text">
-                        ad   afsaaafdsfsafdf
-
-                    </div>
-                </div>
-                <div className="juzhong">
-                    <div className="juzhong1">
-                        div也会被line-height居中
-                    </div>
-                </div>
-                <div    className={classnames('no-active', {
-                    active: '2' === '1',
-                })}>
-sclas
-                </div>
+            <div className="All-div" onClick={this.handleClick}>
+                <p>redux的值{this.props.counter}</p>
+                <button onClick={this.handleOn}>加一个redux的值</button>
+                <p className="p-point">{this.state.change+''}</p>
             </div>
         );
     }
 }
+
+export default Test
