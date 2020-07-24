954bf712215ba73455ecbc621bc6aaf8240c20e6
caozhengqian <1264419544@qq.com>
2020-07-24 19:37:54 +0800

    初始化imoc

--- react-compent/src/comLife/All/index.js
+++ react-compent/src/comLife/All/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./index.less";
+import { Table, Tag, Space ,Button } from 'antd';
 
 import One from "./One";
 import Two from "./Two"
@@ -16,7 +17,78 @@ class All extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            change:true
+            change:true,
+            columns :[
+                {
+                    title: 'Name',
+                    dataIndex: 'name',
+                    key: 'name',
+                    render: (text,record,index) => <span onClick={(e)=>this._name(e,record)}>{text}</span>,
+                },
+                {
+                    title: 'Age',
+                    dataIndex: 'age',
+                    key: 'age',
+                },
+                {
+                    title: 'Address',
+                    dataIndex: 'address',
+                    key: 'address',
+                },
+                {
+                    title: 'Tags',
+                    key: 'tags',
+                    dataIndex: 'tags',
+                    render: tags => (
+                        <>
+                            {tags.map(tag => {
+                                let color = tag.length > 5 ? 'geekblue' : 'green';
+                                if (tag === 'loser') {
+                                    color = 'volcano';
+                                }
+                                return (
+                                    <Tag color={color} key={tag}>
+                                        {tag.toUpperCase()}
+                                    </Tag>
+                                );
+                            })}
+                        </>
+                    ),
+                },
+                {
+                    title: 'Action',
+                    key: 'action',
+                    render: (text, record) => (
+                        <Space size="middle">
+                            <span>Invite {record.name}</span>
+                            <span>Delete</span>
+                        </Space>
+                    ),
+                },
+            ],
+            data : [
+            {
+                key: '1',
+                name: 'John Brown',
+                age: 32,
+                address: 'New York No. 1 Lake Park',
+                tags: ['nice', 'developer'],
+            },
+            {
+                key: '2',
+                name: 'Jim Green',
+                age: 42,
+                address: 'London No. 1 Lake Park',
+                tags: ['loser'],
+            },
+            {
+                key: '3',
+                name: 'Joe Black',
+                age: 32,
+                address: 'Sidney No. 1 Lake Park',
+                tags: ['cool', 'teacher'],
+            },
+        ]
         };
     }
     //新建、更新，都会走
@@ -54,14 +126,36 @@ class All extends React.Component {
     handleOn=()=>{
       this.props.addGun();
     };
+    _table(record,event){
+        console.info("点击了行")
+        console.info(record)
+        console.info(event)
+        event.stopPropagation ()
+    }
+    _button(){
+        console.info("点击了_button")
+
+    }
+    _name(e,re){
+        console.info("点击了_name");
+        console.info(e)
+        console.info(re)
+        e.stopPropagation()
+    }
     render() {
         return (
             <div className="All-div" onClick={this.handleClick}>
                 <p>redux的值{this.props.counter}</p>
                 <button onClick={this.handleOn}>加一个redux的值</button>
                 <p className="p-point">{this.state.change+''}</p>
+                <Button onClick={this._button}>aaa</Button>
                 <One/>
                 <Two/>
+                <Table  onRow={record => {
+                    return {
+                        onClick: (event)=>this._table(record,event)// 点击行
+                    };
+                }} columns={this.state.columns} dataSource={this.state.data} />
             </div>
         );
     }
