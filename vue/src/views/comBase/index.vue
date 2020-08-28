f036b545f30ce95dce0bb62959170a297d13ef32
caozhengqian@deppon.com <poxt271@>
2020-08-28 20:17:15 +0800

    add 表单绑定

--- vue/src/views/comBase/index.vue
+++ vue/src/views/comBase/index.vue
@@ -1,16 +1,24 @@
 <template>
     <div class='ComBase'>
-        <p>ComBase</p>
+       <Test
+         v-for="v in posts"
+         :key="v.id"
+         :title="v.title"
+       />
+        <p>可以直接用v进行循环</p>
+        <p> v-for="<span class="red">v</span> in posts" </p>
+        <p> :key="<span class="red">v</span>.id" </p>
+        <p> :title="<span class="red">v</span>.title" </p>
     </div>
 
 </template>
 
 <script>
     //import { mapState } from "vuex";
-    //import All from "./comLife/All";
+    import Test from "./test";
     export default {
         components: {
-            //       All,
+            Test
         },
         name: 'ComBase',
         props: {
@@ -20,7 +28,13 @@
             // ...mapState(["activityData"])
         },
         data() {
-            return {}
+            return {
+                posts: [
+                    { id: 1, title: 'test1' },
+                    { id: 2, title: 'test2' },
+                    { id: 3, title: 'test3' }
+                ]
+            }
         },
         created() {
 
@@ -35,6 +49,8 @@
 
 <style lang="less" scoped>
     .ComBase {
-
+        .red{
+            color:red
+        }
     }
 </style>
