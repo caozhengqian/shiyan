073b7f2a5d3032c3bbdbe1bc16791780f106f245
1264419544@qq.com <1264419544@qq.com>
2020-10-15 21:00:51 +0800

    add elementui增加接口

--- vue/src/api/table/table.js
+++ vue/src/api/table/table.js
@@ -1,23 +1,21 @@
-import  {_products} from "./data.js"
+import  {table1} from "./data.js"
 const timeOut = 2000;//所有接口延迟时间
 const fetchOk = true;//所有接口正确返回？
-export default {
-    newProducts (params){
+export function getTableData (params){
         console.info("请求参数==>:",params);
         return  new Promise((resolve, reject) => {
             try{
                 if(fetchOk){
                     setTimeout(()=>{
-                        resolve({code:'200',_products});
-                    },timeOut)
+                        resolve({code:'200',data:table1});
+                    },2000 || timeOut)
                 }else{
                     setTimeout(()=>{
                         resolve({code:'503',msg:"失败！"});
-                    },timeOut)
+                    },2000 || timeOut)
                 }
             } catch (error) {
                 reject(error)
             }
         });
-    }
 }
--- vue/src/compents/elements/data/Etable/Etable.vue
+++ vue/src/compents/elements/data/Etable/Etable.vue
@@ -1,12 +1,31 @@
 <template>
     <div class='Etable'>
-        <p>Etable</p>
+        <p>基本</p>
+        <el-table
+                :data="tableData"
+                border
+                style="width: 100%">
+            <el-table-column
+                    prop="date"
+                    label="日期"
+                    width="180">
+            </el-table-column>
+            <el-table-column
+                    prop="name"
+                    label="姓名"
+                    width="180">
+            </el-table-column>
+            <el-table-column
+                    prop="address"
+                    label="地址">
+            </el-table-column>
+        </el-table>
     </div>
 </template>
 
 <script>
     //import { mapState } from "vuex";
-    //import All from "./comLife/All";
+    import {getTableData} from "../../../../api/table/table";
     export default {
         components: {
             //       All,
@@ -19,11 +38,16 @@
             // ...mapState(["activityData"])
         },
         data() {
-            return {}
+            return {
+                tableData:null//请求返回的结果
+            }
         },
         created() {
-
+            getTableData("请求").then((res)=>{
+                this.tableData = res.data
+            });
         },
+        
         methods: {
             aa() {
             },
