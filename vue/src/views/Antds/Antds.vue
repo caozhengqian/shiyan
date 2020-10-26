87ec467b5c3fd31d718ba6a0dc620bd22b74c915
1264419544@qq.com <1264419544@qq.com>
2020-10-26 18:35:46 +0800

    test git

--- vue/src/views/Antds/Antds.vue
+++ vue/src/views/Antds/Antds.vue
@@ -50,7 +50,7 @@
 <style lang="less" scoped>
     .Antds {
         .title{
-            font-size: 19px;
+            font-size: 20px;
             color:blue;
             padding-bottom: 10px;
             cursor: pointer;
