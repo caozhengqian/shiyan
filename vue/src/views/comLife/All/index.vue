3fd03ad18bf915129331d2676a1b0a9dc0df93e9
1264419544@qq.com <1264419544@qq.com>
2020-11-03 15:12:29 +0800

    add connectObj

--- vue/src/views/comLife/All/index.vue
+++ vue/src/views/comLife/All/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+      <p>vue在每个周期里更改this的data。created打印data是最终的结果</p>
     <p @click="aa()">All
       {{ change }}</p>
     <One :change="change" />
@@ -8,6 +9,7 @@
 </template>
 
 <script>
+let ast = { 'a': 'aa' }
 // import { mapState } from "vuex";
 import One from './One'
 import Two from './Two'
@@ -22,7 +24,8 @@ export default {
   },
   data() {
     return {
-      change: true
+      change: true,
+      tmp: { a: 'aa' }
     }
   },
   computed: {
@@ -33,24 +36,52 @@ export default {
   },
   created() {
     console.info('=>All-created')
+    this.tmp.a = 'created'
+    ast.a = 'created'
+    console.info('this.tmp===', this.tmp)
+    console.info('ast===', ast)
   },
   beforeMount() {
     console.info('=>All-beforeMount')
+    this.tmp.a = 'beforeMount'
+    ast.a = 'beforeMount'
+    console.info('this.tmp===', this.tmp)
+    console.info('ast===', ast)
   },
   mounted() {
     console.info('=>All-mounted')
+    this.tmp.a = 'mounted'
+    ast.a = 'mounted'
+    console.info('this.tmp===', this.tmp)
+    console.info('ast===', ast)
   },
   beforeUpdate() {
     console.info('=>All-beforeUpdate')
+    this.tmp.a = 'beforeUpdate'
+    ast.a = 'beforeUpdate'
+    console.info('this.tmp===', this.tmp)
+    console.info('ast===', ast)
   },
   updated() {
     console.info('=>All-updated')
+    this.tmp.a = 'updated'
+    ast.a = 'updated'
+    console.info('this.tmp===', this.tmp)
+    console.info('ast===', ast)
   },
   beforeDestroy() {
     console.info('=>All-beforeDestroy')
+    this.tmp.a = 'beforeDestroy'
+    ast.a = 'beforeDestroy'
+    console.info('this.tmp===', this.tmp)
+    console.info('ast===', ast)
   },
   destroyed() {
     console.info('=>All-destroyed')
+    this.tmp.a = 'destroyed'
+    ast.a = 'destroyed'
+    console.info('this.tmp===', this.tmp)
+    console.info('ast===', ast)
   },
   methods: {
     aa() {
