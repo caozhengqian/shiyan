0db9a2d6c13cbf4558f6f79a3d1f21f88dc5ec59
1264419544@qq.com <1264419544@qq.com>
2020-11-22 18:32:21 +0800

    add vue2.6组件生命周期问题。

--- vue/src/views/comLife/All/index.vue
+++ vue/src/views/comLife/All/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
       <p>vue在每个周期里更改this的data。created打印data是最终的结果</p>
+      <p>父组件creat用async，只能在本组件async，组织不了子组件里面created</p>
+      <p>父组件异步改变传给子组件的值，子组件watch该值会先执行，再执行父组件异步结束</p>
+      <p>父穿[]给子组件，父组件异步赋值给[]，子组件不更新</p>
     <p @click="aa()">All
       {{ change }}</p>
     <One :change="change" />
